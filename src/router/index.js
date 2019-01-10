efa4ffeaf03d5af6f250d388718fac78b4c8be76
张洪影 <l>
2019-01-10 18:22:14 +0800

    业绩tab

--- src/router/index.js
+++ src/router/index.js
@@ -62,7 +62,7 @@ export default new Router({
     },
     {
       path:'/',
-      redirect:'/login'
+      redirect:'/msite'
     },
     ...XQhome
   ]
--- src/xqPages/pages/achievement/index.vue
+++ src/xqPages/pages/achievement/index.vue
@@ -1,5 +1,19 @@
 <template>
-    <div>业绩</div>
+    <div>
+      <div class="title_wrap">
+        <span>业绩标题</span>
+      </div>
+    <div class="tab_wrap">
+      <ul>
+        <li><span>tab1</span></li>
+        <li><span>tab1</span></li>
+        <li><span>tab1</span></li>
+         <li><span>tab1</span></li>
+          <li><span>tab1</span></li>
+           <li><span>tab1</span></li>
+      </ul>
+    </div>
+    </div>
 </template>
 
 <script>
@@ -14,6 +28,37 @@
   }
 </script>
 
-<style scoped>
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+ .title_wrap
+   width 100%
+   height 50px
+   background-color #e6e6e6
+   display flex
+   justify-content center
+   align-items center
+   span  
+    font-size 18px
+ .tab_wrap
+  width 100%
+  height 50px
+  background-color #f2d7a8
+  ul 
+   list-style none
+   height 100%
+   display flex
+   justify-content center
+   align-items center
+   li 
+    flex 1
+    height 100%
+    line-height 50px
+    text-align center
+  
+ 
+
+    
+
+
+
 
 </style>
